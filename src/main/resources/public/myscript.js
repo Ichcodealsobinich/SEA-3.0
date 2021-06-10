9b502b7eba05e8899496b79b3da1578c718631b0
sea4 <f.leonhardt@telekom.de>
2021-06-10 16:10:04 +0200

    Load Json with Javascript and dynamically build table

---
+++ src/main/resources/public/myscript.js
@@ -0,0 +1,45 @@
+var para = document.getElementById("2");
+para.textContent = "List of Persons";
+
+
+
+//fetch("http://localhost:8080/personen.json")
+//	.then(answer => answer.json()) 	//Lamnda-Ausdruck statt Funktionsparameter
+//	.then(json => cell.textContent = json.Personen[0].anrede); //Lamnda-Ausdruck statt Funktionsparameter
+
+fetch("http://localhost:8080/personen.json")
+	.then(getJson)     //Functional programming in JS
+	.then(buildTable); //Functional programming in JS
+
+function getJson(ServerResponse){
+	return ServerResponse.json();
+}
+
+function buildTable(json) {
+	var tabelle = document.getElementById("idtable");
+	var i =0;
+	for (var element of json.Personen) {
+		tabelle.insertAdjacentHTML("beforeend", 
+			  "<tr>"
+			+ 	`<th scope='row'>  ${i++}   </th>`
+			+	`<td> ${element.anrede}</td>`
+			+	`<td> ${element.vorname}</td>`
+			+	`<td> ${element.nachname}</td>`
+			+	`<td><img src='${getIcon(element.anrede)}'></td>`
+			+ "</tr>"			
+		);
+	}
+}
+
+function getIcon(anrede) {
+	switch(anrede) {
+  		case "Herr":
+    		return 'img/mann.png'
+  		case "Frau":
+    		return 'img/frau.png'
+  		default:
+    		return 'img/frau.png'
+	} 
+}
+
+
